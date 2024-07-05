3116f2a003dbb94775911b6fc314b3d98e720569
MarcLecha <marc.lecha@datapta.com>
2024-07-05 11:29:50 +0200

    compilador mejorado con instalacion de dependencias automaticas

--- build-tools/buildElectronApp.cjs
+++ build-tools/buildElectronApp.cjs
@@ -1,8 +1,9 @@
-const { remove, ensureDir, copy } = require('fs-extra');
-const { join } = require('path');
+const { remove, ensureDir, copy, pathExists } = require('fs-extra');
 const packager = require('electron-packager');
 const { createPackage } = require('asar');
+const { join } = require('path');
 
+const { installDependencies } = require('./installDependencies.cjs');
 
 const projectRoot = join(__dirname, '..');
 const { name, version, author } = require(join(projectRoot, 'package.json'));
@@ -11,8 +12,24 @@ const outDir = join(__dirname, 'build');
 const outDirSrc = join(outDir, 'src');
 const srcDir = join(__dirname, '..', 'src');
 
-async function build() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function buildElectronApp() {
   try {
+    // Verificar la existencia de package.json y node_modules
+    const [packageJsonExiste, nodeModulesExiste] = await Promise.all([
+      pathExists(join(__dirname, 'package.json')),
+      pathExists(join(__dirname, 'node_modules'))
+    ]);
+
+    if (!packageJsonExiste || !nodeModulesExiste) {
+      await installDependencies();
+      await sleep(2000);
+    }
+
+    // Limpiar y asegurar el directorio de salida
     await remove(outDir);
     await ensureDir(outDir);
 
@@ -26,7 +43,7 @@ async function build() {
     // Ejecutar electron-packager
     const appPaths = await packager({
       dir: outDir,
-      out: join(__dirname, '..', 'dist'), // Corregido el path de salida
+      out: join(__dirname, '..', 'dist'),
       overwrite: true,
       platform: 'win32',
       arch: 'x64',
@@ -48,10 +65,14 @@ async function build() {
     // Limpiar el directorio temporal
     await remove(outDir);
 
+    console.log('');
+    console.log('******************************');
     console.log('Build completed successfully!');
+    console.log('******************************');
+    console.log('');
   } catch (error) {
     console.error('Error during build:', error);
   }
 }
 
-build();
+buildElectronApp();
---
+++ build-tools/installDependencies.cjs
@@ -0,0 +1,31 @@
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+const { exec } = require('child_process');
+
+const projectRoot = join(__dirname, '..');
+const { main, dependencies } = require(join(projectRoot, 'package.json'));
+const newPackageJson = {
+    main: main,
+    dependencies: dependencies
+};
+
+const newPackagePath = join(__dirname, 'package.json');
+
+async function installDependencies() {
+    console.log('Installing dependencies...');
+    writeFileSync(newPackagePath, JSON.stringify(newPackageJson, null, 2), 'utf8');
+    exec('npm install', { cwd: join(__dirname) }, (error, stdout, stderr) => {
+        if (error) {
+            throw new Error(`Error installing dependencies: ${error.message}`);
+        }
+        if (stderr) {
+            throw new Error(`Error in install output: ${stderr}`);
+        }
+        
+        console.log('Dependencies installed successfully:');
+        console.log(dependencies)
+        console.log('');
+    });
+}
+
+module.exports = { installDependencies };
