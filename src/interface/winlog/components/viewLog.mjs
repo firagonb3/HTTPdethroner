b185a8b2ed8eab47afa09ac664f6916147b426cc
MarcLecha <marc.lecha@datapta.com>
2024-09-17 15:58:27 +0200

    fix scroll in viewLog.mjs

--- src/interface/winlog/components/viewLog.mjs
+++ src/interface/winlog/components/viewLog.mjs
@@ -1,19 +1,39 @@
 import { $ } from "../../../utils/ECMAScript/index.mjs"
 export function viewLog() {
 
+    function scrollToBottom() {
+        const container = document.getElementById('log-container');
+        container.scrollTop = container.scrollHeight;
+    }
+
     window.logMessage.onLogMessage((message) => {
-        console.log(message)
         const { type, args } = message;
         $('#log-container').appendChild('p', `[${type.toUpperCase()}] ${args.join(' ')}`)
+        scrollToBottom();
     });
 
     return /* html */`
         <serviceControll-Log>
-                <p>
-                    log:
-                </p>
-                <serviceControll-Box id="log-container">
-                </serviceControll-Box>
+            <serviceControll-Box id="log-container">
+            </serviceControll-Box>
         </serviceControll-Log>
+
+        <style>
+            serviceControll-log p {
+                margin: 0;
+                padding: 0.5em;
+            }
+
+            serviceControll-box {
+                width: 100vw;
+                height: 90vh;
+                overflow-y: auto;
+                box-sizing: border-box;
+                position: relative;
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+            }
+        </style>
     `;
 }
--- src/windata.cjs
+++ src/windata.cjs
@@ -2,37 +2,40 @@
 const windata = {
     "winMain": {
         id: "winMain", 
-        width: 1000,
-        height: 500,
+        // width: 1000,
+        // height: 500,
 
-        // width: 410,
-        // height: 430,
+        width: 410,
+        height: 430,
 
         loadURL: true,
         loadFile: 'winMain/index.html',
 
-        resizableCSS: false,
+        resizableCSS: true,
 
-        frame: true,
-        transparent: false,
-        maximizable: true,
-        resizable: true,
-        devtools: true
+        // frame: true,
+        // transparent: false,
+        // maximizable: true,
+        // resizable: true,
+        // devtools: true
 
-        // frame: false,
-        // transparent: true,
-        // maximizable: false,
-        // resizable: false,
-        // devtools: false
+        frame: false,
+        transparent: true,
+        maximizable: false,
+        resizable: false,
+        devtools: false
 
 
     },
     "winLog": {
         id: "winLog",
-        width: 1000,
-        height: 430,
+        // width: 1000,
+        // height: 430,
         loadURL: true,
         loadFile: 'winlog/index.html',
+
+        width: 410,
+        height: 430,
 
         resizableCSS: true,
 
